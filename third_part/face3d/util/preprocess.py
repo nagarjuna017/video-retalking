45624daa6b7fde00f40cde5d7af29904600d3337
nagarjuna017 <96438446+nagarjuna017@users.noreply.github.com>
2024-02-19 17:51:51 +0530

    Update preprocess.py

--- third_part/face3d/util/preprocess.py
+++ third_part/face3d/util/preprocess.py
@@ -35,7 +35,7 @@ def POS(xp, x):
     sTx = k[3]
     sTy = k[7]
     s = (np.linalg.norm(R1) + np.linalg.norm(R2))/2
-    t = np.array([sTx, sTy])  # Convert tuple to NumPy array
+    t = np.stack([sTx, sTy], axis=0)
 
     return t, s
 
@@ -169,7 +169,7 @@ def extract_5p(lm):
 def align_img(img, lm, lm3D, mask=None, target_size=224., rescale_factor=102.):
     """
     Return:
-        transparams        --numpy.array  (raw_W, raw_H, scale, tx, ty)
+        trans_params        --numpy.array  (raw_W, raw_H, scale, tx, ty)
         img_new            --PIL.Image  (target_size, target_size, 3)
         lm_new             --numpy.array  (68, 2), y direction is opposite to v direction
         mask_new           --PIL.Image  (target_size, target_size)
@@ -180,7 +180,6 @@ def align_img(img, lm, lm3D, mask=None, target_size=224., rescale_factor=102.):
         lm3D               --numpy.array  (5, 3)
         mask               --PIL.Image  (raw_H, raw_W, 3)
     """
-
     w0, h0 = img.size
     if lm.shape[0] != 5:
         lm5p = extract_5p(lm)
@@ -193,6 +192,11 @@ def align_img(img, lm, lm3D, mask=None, target_size=224., rescale_factor=102.):
 
     # processing the image
     img_new, lm_new, mask_new = resize_n_crop_img(img, lm, t, s, target_size=target_size, mask=mask)
+    
+    # Convert t to numpy array
+    t = np.array(t)
+    
+    # Construct trans_params
     trans_params = np.array([w0, h0, s, t[0], t[1]])
 
     return trans_params, img_new, lm_new, mask_new
